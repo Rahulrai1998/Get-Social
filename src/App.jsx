58eddadf47ba752ff6d8d59ef6887738b0183a38
Rahul Kumar <rahulrai2822march@gmail.com>
2025-08-25 20:26:20 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,16 @@ import Footer from "./components/Footer";
 import Sidebar from "./components/Sidebar";
 import CreatePostForm from "./components/CreatePostForm";
 import PostList from "./components/PostList";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import PostListProvider from "./store/PostListProvider";
 import CreatePostHome from "./components/CreatePostHome";
 
 function App() {
   const [selectedTab, setSelectedTab] = useState("Home");
 
-  const handleTabChange = (tabName) => {
+  const handleTabChange = useCallback((tabName) => {
     setSelectedTab(tabName);
-  };
+  }, []);
 
   return (
     <PostListProvider>
--- src/store/PostListProvider.jsx
+++ src/store/PostListProvider.jsx
@@ -36,32 +36,32 @@ const postListReducer = (state, { type, payload }) => {
   }
 };
 
-const DEFAULT_POST_LIST = [
-  {
-    id: "1",
-    title: "Vote for me",
-    body: "I request you all to vote for me.",
-    reactions: 3,
-    userId: "user-9",
-    tags: ["vote", "election", "votForChange"],
-  },
-  {
-    id: "2",
-    title: "New Vacation",
-    body: "On my way to explore the nature.",
-    reactions: 6,
-    userId: "user-1",
-    tags: ["explore", "nature"],
-  },
-  {
-    id: "3",
-    title: "ManU won",
-    body: "Champions league final was just excititng.",
-    reactions: 10,
-    userId: "user-2",
-    tags: ["football", "uefa", "winners"],
-  },
-];
+// const DEFAULT_POST_LIST = [
+//   {
+//     id: "1",
+//     title: "Vote for me",
+//     body: "I request you all to vote for me.",
+//     reactions: 3,
+//     userId: "user-9",
+//     tags: ["vote", "election", "votForChange"],
+//   },
+//   {
+//     id: "2",
+//     title: "New Vacation",
+//     body: "On my way to explore the nature.",
+//     reactions: 6,
+//     userId: "user-1",
+//     tags: ["explore", "nature"],
+//   },
+//   {
+//     id: "3",
+//     title: "ManU won",
+//     body: "Champions league final was just excititng.",
+//     reactions: 10,
+//     userId: "user-2",
+//     tags: ["football", "uefa", "winners"],
+//   },
+// ];
 
 const PostListProvider = ({ children }) => {
   //WE ARE USING CONTEXT HERE FOR THE PURPOSE OF GLOBAL STATE MANAGEMENT
